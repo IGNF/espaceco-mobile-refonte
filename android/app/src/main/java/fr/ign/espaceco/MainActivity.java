2440bd6618f311e7765b3376e8ead3e83c294276
Alex0x47 <grisey.alexandre@gmail.com>
2026-01-05 11:39:39 +0100

    setup React & Capacitor project and dependencies

---
+++ android/app/src/main/java/fr/ign/espaceco/MainActivity.java
@@ -0,0 +1,5 @@
+package fr.ign.espaceco;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'fr.ign.espaceco',
+  appName: 'espaceco',
+  webDir: 'dist'
+};
+
+export default config;
