2fb558347702daf2d4af17f0e9fb26d68f97202b
Alex0x47 <grisey.alexandre@gmail.com>
2026-01-07 09:49:10 +0100

    install mobile-core and mobile-device dependencies

--- src/domain/user/models.ts
+++ src/domain/user/models.ts
@@ -1,17 +1,4 @@
-/**
- * What to do here is import directly the types from the mobile-core module
- * 
- * Have something maybe like:
- * 
- * export EspaceCo_User extends User {} // in case we need more property than what the module offers
- */
-
-export interface User {
-  id: string;
-  username: string;
-  email?: string;
-  firstName?: string;
-  lastName?: string;
-  // communities: Community[];
-  // communities_member?: CommunityMember[];
+import type { User } from '@ign/mobile-core';
+export interface AppUser extends User { // in case we need more property than what the module offers
+  appData: object;
 }
