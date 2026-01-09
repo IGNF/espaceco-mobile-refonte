55c52641e92ea496db5f1890849f4d216a609338
Alex0x47 <grisey.alexandre@gmail.com>
2026-01-09 11:06:46 +0100

    add ol package, display map on the home page, and adjust floating elements

---
+++ src/shared/constants/map.ts
@@ -0,0 +1,4 @@
+export const DEFAULT_MAP_CENTER_LON_LAT: [number, number] = [2.3522, 48.8566]
+export const DEFAULT_MAP_ZOOM = 11
+export const DEFAULT_MAP_FOCUS_ZOOM = 13
+export const DEFAULT_MAP_SHOW_SCALELINE = true
