8f5494d349762c04bfdafc1ff110536a726c39c4
Alex0x47 <grisey.alexandre@gmail.com>
2026-01-14 11:01:02 +0100

    add external link const file, and improve externallink component style

---
+++ src/shared/constants/externalLinks.ts
@@ -0,0 +1,7 @@
+/**
+ * External links used throughout the application.
+ * Centralized here for easier maintenance.
+ */
+export const EXTERNAL_LINKS = {
+  ESPACE_COLLABORATIF: 'https://espacecollaboratif.ign.fr/',
+} as const;
