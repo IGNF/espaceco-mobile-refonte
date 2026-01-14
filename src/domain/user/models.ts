bf69e43e37f9ce0d027850d2d8c2edd835e5d6d1
Alex0x47 <grisey.alexandre@gmail.com>
2026-01-14 11:00:30 +0100

    edit user model, and fix css from login page

--- src/domain/user/models.ts
+++ src/domain/user/models.ts
@@ -1,6 +1,7 @@
 import type { User } from '@ign/mobile-core';
 export interface AppUser extends User { // in case we need more property than what the module offers
   isAnonymous?: boolean;
-  avatarUrl?: string;
+  avatarUrl?: string; // see if this should be moved to the User from mobile-core
+  description?: string; // see if this should be moved to the User from mobile-core
   appData?: object;
 }
