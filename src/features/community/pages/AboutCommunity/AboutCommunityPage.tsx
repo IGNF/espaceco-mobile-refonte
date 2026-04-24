4dd0d7fd013253d20d31fc70b7a456c6f2fa4d68
Alex0x47 <grisey.alexandre@gmail.com>
2026-04-24 16:12:13 +0200

    create about community page

--- src/features/community/pages/AboutCommunity/AboutCommunityPage.tsx
+++ src/features/community/pages/AboutCommunity/AboutCommunityPage.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from "react-i18next";
 
 import screen from '@/shared/styles/screen.module.css';
 import typography from '@/shared/styles/typography.module.css';
+import { useCommunity } from "../../hooks/useCommunity";
 
 export interface AboutCommunityPageProps {
   isOpen: boolean;
@@ -12,13 +13,15 @@ export interface AboutCommunityPageProps {
 
 export function AboutCommunityPage({ isOpen, onClose }: AboutCommunityPageProps) {
 	const { t } = useTranslation();
+  const { activeCommunity } = useCommunity();
 
 	return (
 		<SlideUpPage isOpen={isOpen} onClose={onClose}>
-			<PageHeader title="About Community" onClose={onClose} />
+			<PageHeader title={t('aboutCommunity.title')} subtitle={activeCommunity?.name ?? ''} onClose={onClose} />
 			<main className={screen.screenContainer}>
 				<h1 className={typography.title}>{t('aboutCommunity.title')}</h1>
 				<p className={typography.subtitle}>{t('aboutCommunity.subtitle')}</p>
+        <p className={typography.paragraph}>{t('aboutCommunity.description')}</p>
 			</main>
 		</SlideUpPage>
 	);
